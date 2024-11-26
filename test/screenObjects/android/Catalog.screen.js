75b03ba39a5525b04430d1ac49683e4afe911cac
Leonardo <leonardo.yaranga@gmail.com>
2024-11-25 23:19:56 -0500

    new testcases product

--- test/screenObjects/android/Catalog.screen.js
+++ test/screenObjects/android/Catalog.screen.js
@@ -4,6 +4,16 @@ class CatalogScreen {
       'android=new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productTV")'
     );
   }
+  //Product 1
+  //xpath: (//android.widget.ImageView[@content-desc="Product Image"])[1]
+  //-android uiautomator: new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productIV").instance(0)
+  get sauceLabsBackpackProduct(){
+    return $(
+      'android=new UiSelector().resourceId("com.saucelabs.mydemoapp.android:id/productIV").instance(0)'
+    )
+  }
+
+
 }
 
 module.exports = new CatalogScreen();
