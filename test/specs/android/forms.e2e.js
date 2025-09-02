fd7cbb654f815033d116d2c71c1ad5ae2ecab75b
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-09-01 19:53:14 -0600

    feat: form test suite

--- test/specs/android/forms.e2e.js
+++ test/specs/android/forms.e2e.js
@@ -1,6 +1,6 @@
 describe("My Login Demo", () => {
   beforeEach(async () => {
-    //Access the hamburguer/toggle button by its accessibility id
+    // Tap new" command -> https://webdriver.io/docs/api/mobile/tap
     await $("~Forms").tap();
   });
 
@@ -13,4 +13,42 @@ describe("My Login Demo", () => {
       )
     ).toHaveText("Hello from Appium!");
   });
+
+  it('should turn switch on and off', async () => {
+    await $('~switch').tap();
+    await expect($('~switch-text')).toHaveText("Click to turn the switch OFF");
+    await $('~switch').tap();
+    await expect($('~switch-text')).toHaveText("Click to turn the switch ON");
+  })
+
+  it('should select a dropdown item', async () => {
+    await $('//*[@text="Select an item..."]').tap();
+    await $('//android.widget.CheckedTextView[@resource-id="android:id/text1" and @text="This app is awesome"]').tap();
+    await expect($('//android.widget.EditText[@text="This app is awesome"]')).toHaveText("This app is awesome");
+  })
+
+  it('should be able to click on the active button and then close the modal', async () => {
+    await $('//android.widget.TextView[@text="Active"]').tap();
+
+    // Wait for the dialog message to appear
+    const dialogMessage = await $('//*[@resource-id="android:id/message"]');
+    await dialogMessage.waitForDisplayed();
+    await expect(dialogMessage).toHaveText("This button is active");
+
+    // Click OK button to close the modal
+    const okButton = await $('//*[@resource-id="android:id/button1"]');
+    await okButton.tap();
+
+    // Verify modal is closed by checking the message is no longer displayed
+    await expect(dialogMessage).not.toBeDisplayed();
+  })
+
+  it('should be able to click on the inactive button and confirm the modal is not displayed', async () => {
+    await $('//android.widget.TextView[@text="Inactive"]').tap();
+
+    // Verify no modal dialog message appears after clicking inactive button
+    const dialogMessage = $('//*[@resource-id="android:id/message"]');
+    await expect(dialogMessage).not.toBeDisplayed();
+  })
+
 });
